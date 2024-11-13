3780573f6ec68c9f5d7e1bd2c28932fbf0683754
Maesawa-code <hirokitechcamp@gmail.com>
2024-11-13 18:41:34 +0900

    販売価格の計算

---
+++ app/javascript/item_price.js
@@ -0,0 +1,25 @@
+const price = () => {
+
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    console.log(inputValue);
+  
+    const addTaxDom = document.getElementById("add-tax-price");
+    const profitDom = document.getElementById("profit");
+  
+    if (inputValue) {
+      const tax = Math.floor(inputValue * 0.1); 
+      const profit = inputValue - tax; 
+  
+      addTaxDom.innerHTML = tax; 
+      profitDom.innerHTML = profit; 
+    } else {
+      addTaxDom.innerHTML = "0";
+      profitDom.innerHTML = "0";
+    }
+  });
+ };
+ 
+ window.addEventListener("turbo:load", price);
+ window.addEventListener("turbo:render", price);
