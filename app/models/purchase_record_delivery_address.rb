e8a264914b10f8e2a86b2e97bc19ea1a113d9473
Maesawa-code <hirokitechcamp@gmail.com>
2024-11-18 12:54:18 +0900

    バリデーションと保存の処理の追加

--- app/models/purchase_record_delivery_address.rb
+++ app/models/purchase_record_delivery_address.rb
@@ -1,4 +1,27 @@
 class PurchaseRecordDeliveryAddress
   include ActiveModel::Model
   attr_accessor :item, :user, :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number, :purchase_record
+
+  with_options presence: true do
+    validates :item_id
+    validates :user_id
+    validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :city
+    validates :address
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid' }
+  end
+
+  def save
+    purchase_record = PurchaseRecord.create(user_id: user_id, item_id: item_id)
+    DeliveryAddress.create(
+      postal_code: postal_code,
+      prefecture_id: prefecture_id,
+      city: city,
+      address: address,
+      building_name: building_name,
+      phone_number: phone_number,
+      purchase_record_id: purchase_record.id
+    )
+  end
 end
