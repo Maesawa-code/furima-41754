9828235a5b15b0167cd32fc19fe2cff80a98b8d0
Maesawa-code <hirokitechcamp@gmail.com>
2024-11-19 19:00:55 +0900

    card.jsのフォーム取得方法の修正

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,8 +1,7 @@
 const pay = () => {
   const publicKey = gon.public_key
-  const payjp = Payjp(publicKey) 
+  const payjp = Payjp(publicKey)
   const elements = payjp.elements();
-
   const numberElement = elements.create('cardNumber');
   const expiryElement = elements.create('cardExpiry');
   const cvcElement = elements.create('cardCvc');
@@ -10,33 +9,24 @@ const pay = () => {
   numberElement.mount('#number-form');
   expiryElement.mount('#expiry-form');
   cvcElement.mount('#cvc-form');
-
   const form = document.getElementById('charge-form')
-
-  form.addEventListener('submit', (e) => {
-    e.preventDefault(); 
-
-    payjp.createToken(numberElement).then((response) => {
+  form.addEventListener("submit", (e) => {
+    payjp.createToken(numberElement).then(function (response) {
       if (response.error) {
       } else {
         const token = response.id;
-
-        const tokenInput = document.createElement('input');
-        tokenInput.setAttribute('type', 'hidden');
-        tokenInput.setAttribute('name', 'token');
-        tokenInput.setAttribute('value', token);
-        form.appendChild(tokenInput);
-
-        numberElement.clear();
-        expiryElement.clear();
-        cvcElement.clear();
-
-        form.submit();
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} name='token' type="hidden">`;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
       }
+      numberElement.clear();
+      expiryElement.clear();
+      cvcElement.clear();
+      document.getElementById("charge-form").submit();
     });
+    e.preventDefault();
   });
 };
 
-window.addEventListener('turbo:load', pay);
+window.addEventListener("turbo:load", pay);
 window.addEventListener("turbo:render", pay);
-
