0bb0a6577c5e923963daca16fe7991cb9903016e
Maesawa-code <hirokitechcamp@gmail.com>
2024-11-19 15:52:10 +0900

    クレジットカード決済機能の実装

--- app/controllers/purchase_record_controller.rb
+++ app/controllers/purchase_record_controller.rb
@@ -2,15 +2,18 @@ class PurchaseRecordController < ApplicationController
   before_action :set_item, only: [:index, :create]
 
   def index
+    gon.public_key = ENV['PAYJP_PUBLIC_KEY']
     @purchase_record_delivery_address = PurchaseRecordDeliveryAddress.new
   end
 
   def create
     @purchase_record_delivery_address = PurchaseRecordDeliveryAddress.new(purchase_params)
     if @purchase_record_delivery_address.valid?
+      pay_item
       @purchase_record_delivery_address.save
       redirect_to root_path
     else
+      gon.public_key = ENV['PAYJP_PUBLIC_KEY']
       render :index, status: :unprocessable_entity
     end
   end
@@ -25,5 +28,14 @@ class PurchaseRecordController < ApplicationController
     params.require(:purchase_record_delivery_address).permit(
       :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number
     ).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: purchase_params[:token],
+      currency: 'jpy'
+    )
   end
 end
